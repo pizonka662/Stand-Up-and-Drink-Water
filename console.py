28dc59e81797f6605181cd739e606c525582f016
pizonka662 <pizonka662@gmail.com>
2024-09-02 15:06:02 -0500

    read from serial

--- console.py
+++ console.py
@@ -1,1 +1,12 @@
-print("hello world")
+import serial
+
+def readFromSerial(comport, baudrate):
+
+    ser = serial.Serial(comport, baudrate, timeout=0.1)
+    while True:
+        data = ser.readline().decode().strip()
+        if data:
+            print(data)
+
+if __name__ == '__main__':
+    readFromSerial('COM3', 115200)
